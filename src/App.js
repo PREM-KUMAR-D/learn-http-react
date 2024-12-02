098c7e2c93be3190deb4f5d41b49087e2121e980
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-12-02 06:12:26 +0530

    added the delete movie handler

--- src/App.js
+++ src/App.js
@@ -13,22 +13,24 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-8b263-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+      const loadedMovies = [];
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      for(const key in data){
+        loadedMovies.push({
+          id:key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,14 +41,48 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+
+  async function deleteMovieHandler(id){
+
+    try {
+      const response = await fetch(`https://react-http-8b263-default-rtdb.asia-southeast1.firebasedatabase.app/movies/${id}.json`, {
+        method: 'DELETE',
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to delete the movie');
+      }
+      console.log(movies);
+      setMovies((prev)=> prev.filter((movie)=> movie.id!== id));
+      console.log(movies);
+
+      console.log('Movie deleted successfully');
+    } catch (error) {
+      console.error(error.message);
+    }
+
+
+  }
+
+
+  async function addMovieHandler(movie) {
+    const res = await fetch('https://react-http-8b263-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',{
+      method:'POST',
+      body: JSON.stringify(movie),
+      headers:{
+        'Content-type': 'application/json'
+      }
+    })
+
+    const data = res.json();
+    console.log(data);
+
   }
 
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
+    content = <MoviesList movies={movies}  onMovieDelete={deleteMovieHandler}/>;
   }
 
   if (error) {
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,15 +4,25 @@ import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
+
+  const movieDeleteHandler = (id)=>{
+    props.onMovieDelete(id);
+  }
+
+
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
+        <React.Fragment key={movie.id}>
+          
         <Movie
           key={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
-        />
+          />
+          <button onClick={()=>movieDeleteHandler(movie.id)}> Delete Movie</button>
+        </React.Fragment>
       ))}
     </ul>
   );
